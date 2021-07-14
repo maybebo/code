76207664bdc561c509c941b4054569635e1ff1db
maybebo <lwchwqq@gmail.com>
2021-07-14 15:15:19 +0800

    modified:   1.1.c.exe 	new file:   1.4.c.exe 	new file:   1.6.c 	new file:   1.6.c.exe

---
+++ c/c程序设计第四版学习笔记/1.6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("********************\nVery good\n*********************\n");/*这是块注释，
+                                                            这部分不产生目标代码*/
+    return 0;
+}
