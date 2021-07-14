3de8d9eef3a770d29da65cef2b2ab80e85f06867
513515125 <513515125@qq.com>
2021-07-14 10:04:21 +0800

    修改：     1.1 	修改：     1.1.c

--- c/c程序设计第四版学习笔记/1.1.c
+++ c/c程序设计第四版学习笔记/1.1.c
@@ -2,5 +2,13 @@
 int main()
 {
     printf("This is a C program.\n");
+    //c语言允许两种注释
+    //1.以//开始的单行注释
+    //2.以/*开始，以*/结束的块式注释
+    /*
+        但是字符串中的//和/*都不作为注释的开始，而是作为字符串的一部分
+    */
+    printf("//how do you do!\n");
+    printf("/*how do you do!*/\n");
     return 0;
 }
